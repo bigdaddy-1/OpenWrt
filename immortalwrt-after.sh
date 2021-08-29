e9805eecdacc928ce0410da33f2917350f85635b
bigdaddy-1 <56994151+bigdaddy-1@users.noreply.github.com>
2021-08-29 11:30:20 +0800

    Update immortalwrt-after.sh

--- immortalwrt-after.sh
+++ immortalwrt-after.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# Modify default NAME
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
