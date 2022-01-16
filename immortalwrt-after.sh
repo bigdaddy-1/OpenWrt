030adb0646f11c5d1934b0f63b384b75d12ee5c3
bigdaddy-1 <56994151+bigdaddy-1@users.noreply.github.com>
2022-01-16 11:57:41 +0800

    Update immortalwrt-after.sh

--- immortalwrt-after.sh
+++ immortalwrt-after.sh
@@ -23,3 +23,5 @@ git config core.sparsecheckout true
 echo "luci-app-openclash" >> .git/info/sparse-checkout
 git pull --depth 1 origin master
 git branch --set-upstream-to=origin/master master
+# Modify default dependency
+sed -i 's/uci-firewall/firewall/g' package/feeds/luci/luci-app-firewall/makefile
