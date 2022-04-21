ab588005d5c07b0a7810231e1c6063221e3bebd1
bigdaddy-1 <56994151+bigdaddy-1@users.noreply.github.com>
2022-04-21 23:06:42 +0800

    Update openwrt-after.sh

--- openwrt-after.sh
+++ openwrt-after.sh
@@ -16,17 +16,17 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # Modify default frpc-upx
-rm -rf ./feeds/packages/net/frp
-sed -i 's/PKG_BUILD_DEPENDS:=golang\/host/PKG_BUILD_DEPENDS:=golang\/host upx\/host/g' package/frp/Makefile
-sed -i '44 i \  define Build/Compile\n\t$$(call GoPackage/Build/Compile)\n\t$$(STAGING_DIR_HOST)/bin/upx --lzma --best $$(GO_PKG_BUILD_BIN_DIR)/frpc\n\  endef\n' package/frp/Makefile
+rm -rf ./package/frp
+sed -i 's/PKG_BUILD_DEPENDS:=golang\/host/PKG_BUILD_DEPENDS:=golang\/host upx\/host/g' ./feeds/packages/net/frp/Makefile
+sed -i '43 i define Build/Compile\n\t$$(call GoPackage/Build/Compile)\n\t$$(STAGING_DIR_HOST)/bin/upx --lzma --best $$(GO_PKG_BUILD_BIN_DIR)/frpc\nendef\n' ./feeds/packages/net/frp/Makefile
 
 # Modify default OPENCLASH
-rm -rf ./feeds/luci/applications/luci-app-openclash
-cd ./feeds/luci/applications
+rm -rf ./feeds/luci/applications/luci-app-openclash/*
+cd ./feeds/luci/applications/luci-app-openclash
 git init
 git remote add -f origin https://github.com/vernesong/OpenClash.git
 git config core.sparsecheckout true
 echo "luci-app-openclash" >> .git/info/sparse-checkout
 git pull --depth 1 origin master
 git branch --set-upstream-to=origin/master master
-rm -rf luci-app-openclash/root/etc/openclash/*.* luci-app-openclash/root/etc/openclash/*rule*
+rm -rf ./luci-app-openclash/root/etc/openclash/*.* ./luci-app-openclash/root/etc/openclash/*rule*
