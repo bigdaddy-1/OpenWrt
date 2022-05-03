5a1355c4a9cc51c0ae4b396d94ae8fd27f6db025
bigdaddy-1 <56994151+bigdaddy-1@users.noreply.github.com>
2022-05-04 00:58:28 +0800

    Update openwrt-after.sh

--- openwrt-after.sh
+++ openwrt-after.sh
@@ -18,11 +18,41 @@ sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 # Modify default frpc-upx
 sed -i 's/PKG_SOURCE_DATE:=.*/PKG_SOURCE_DATE:=2021-04-23/g' ./package/openwrt-upx/upx/Makefile
 sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=2dae2a39972bc7c562dd3b02444336050a0b242e/g' ./package/openwrt-upx/upx/Makefile
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=0.42.0/g' ./feeds/packages/net/frp/Makefile
-sed -i 's/PKG_HASH:=.*/PKG_HASH:=4bb815e9c9a4588fce20c6ef33168f0ceb1f420937c4dcf03ce085666328043a/g' ./feeds/packages/net/frp/Makefile
-sed -i '21 i GO_PKG_LDFLAGS:=-s -w' ./feeds/packages/net/frp/Makefile
-sed -i 's/PKG_BUILD_DEPENDS:=golang\/host/PKG_BUILD_DEPENDS:=golang\/host upx\/host/g' ./feeds/packages/net/frp/Makefile
-sed -i '43 i define Build/Compile\n\t$$(call GoPackage/Build/Compile)\n\t$$(STAGING_DIR_HOST)/bin/upx --lzma --best $$(GO_PKG_BUILD_BIN_DIR)/frpc\nendef\n' ./feeds/packages/net/frp/Makefile
+mv ./feeds/packages/net/frp/files ./package/openwrt-frp/
+sed -i '34,66d' ./package/openwrt-frp/Makefile
+sed -i '33 i \\ndefine Package/frp/install\n\t$(call GoPackage/Package/Install/Bin,$(PKG_INSTALL_DIR))\n\n\t$(INSTALL_DIR) $(1)/usr/bin/\n\t$(INSTALL_BIN) $(PKG_INSTALL_DIR)/usr/bin/$(2) $(1)/usr/bin/\n\t$(INSTALL_DIR) $(1)/etc/frp/$(2).d/\n\t$(INSTALL_DATA) $(PKG_BUILD_DIR)/conf/$(2)_full.ini $(1)/etc/frp/$(2).d/\n\t$(INSTALL_DIR) $(1)/etc/config/\n\t$(INSTALL_CONF) ./files/$(2).config $(1)/etc/config/$(2)\n\t$(INSTALL_DIR) $(1)/etc/init.d/\n\t$(INSTALL_BIN) ./files/$(2).init $(1)/etc/init.d/$(2)\n\n\tif [ -r ./files/$(2).uci-defaults ]; then \\\n\t\t$(INSTALL_DIR) $(1)/etc/uci-defaults; \\\n\t\t$(INSTALL_DATA) ./files/$(2).uci-defaults $(1)/etc/uci-defaults/$(2); \\\n\tfi\nendef\ndefine Build/Compile\n\t$$(call GoPackage/Build/Compile)\n\t$$(STAGING_DIR_HOST)/bin/upx --lzma --best $$(GO_PKG_BUILD_BIN_DIR)/frpc\nendef' ./package/openwrt-frp/Makefile
+sed -i 's/PKG_BUILD_DEPENDS:=golang\/host/PKG_BUILD_DEPENDS:=golang\/host upx\/host/g' ./package/openwrt-frp/Makefile
+#sed -i '44 i define Build/Compile\n\t$$(call GoPackage/Build/Compile)\n\t$$(STAGING_DIR_HOST)/bin/upx --lzma --best $$(GO_PKG_BUILD_BIN_DIR)/frpc\nendef\n' ./package/openwrt-frp/Makefile
+cat >> ./package/openwrt-frp/Makefile << \EOF
+define Package/frp/template
+  define Package/$(1)
+    SECTION:=net
+    CATEGORY:=Network
+    SUBMENU:=Web Servers/Proxies
+    TITLE:=$(1) - fast reverse proxy $(2)
+    URL:=https://github.com/fatedier/frp
+    DEPENDS:=$(GO_ARCH_DEPENDS)
+  endef
+
+  define Package/$(1)/description
+    $(1) is a fast reverse proxy $(2) to help you expose a local server behind
+    a NAT or firewall to the internet.
+  endef
+
+  define Package/$(1)/conffiles
+  /etc/config/$(1)
+  endef
+
+  define Package/$(1)/install
+    $(call Package/frp/install,$$(1),$(1))
+  endef
+endef
+
+$(eval $(call Package/frp/template,frpc,client))
+$(eval $(call Package/frp/template,frps,server))
+$(eval $(call BuildPackage,frpc))
+$(eval $(call BuildPackage,frps))
+EOF
 
 # Modify default OPENCLASH
 rm -rf ./feeds/luci/applications/luci-app-openclash/*
