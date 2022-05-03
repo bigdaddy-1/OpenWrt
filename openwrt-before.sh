761fb6cc86a7be1d3c2f87090dcf29b4678d3380
bigdaddy-1 <56994151+bigdaddy-1@users.noreply.github.com>
2022-05-04 00:58:48 +0800

    Update openwrt-before.sh

--- openwrt-before.sh
+++ openwrt-before.sh
@@ -20,7 +20,7 @@
 # 自定义插件
 # Modify default frpc-upx
 git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
-#git clone https://github.com/kuoruan/openwrt-frp.git package/openwrt-frp
+git clone https://github.com/kuoruan/openwrt-frp.git package/openwrt-frp
 
 # Modify default FEEDS
 #mkdir ./package/lean/openclash
