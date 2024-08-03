26cdeebbc041367bb36e246bdf080f59a7e2ee31
vscodebot <vscode@mail.com>
2024-08-04 00:58:11 +0800

    init

---
+++ x86-before.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Modify default OPENCLASH
+mkdir -p ./package/luci-data
+cd ./package/luci-data
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin dev
+git branch --set-upstream-to=origin/dev master
+rm -rf ./luci-app-openclash/root/etc/openclash/*.* ./luci-app-openclash/root/etc/openclash/*rule*
+mv ./luci-app-openclash ..
+
+# Modify default NETDATA
+rm -rf .git
+git init
+git remote add -f origin https://github.com/immortalwrt/luci.git
+git config core.sparsecheckout true
+echo "luci-app-netdata" >> .git/info/sparse-checkout
+git pull origin master
+mv ./applications/luci-app-netdata/ ..
+
+# Modify default Mosdns
+rm -rf .git
+git init
+git remote add -f origin https://github.com/immortalwrt/packages.git
+git config core.sparsecheckout true
+echo "mosdns" >> .git/info/sparse-checkout
+git pull origin master
+mv ./net/mosdns/ ..
